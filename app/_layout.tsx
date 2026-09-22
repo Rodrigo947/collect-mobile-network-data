e0d487352a688422adc5b7db8666b72d4dee072a
Rodrigo Torres <rodrigo.torres@consultores.rnp.br>
2026-09-22 18:00:59 -0300

    Atualizações de bibliotecas e correção de splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 export { ErrorBoundary } from 'expo-router';
@@ -11,6 +12,10 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    useEffect(() => {
+        void SplashScreen.hideAsync();
+    }, []);
+
     return <RootLayoutNav />;
 }
 
